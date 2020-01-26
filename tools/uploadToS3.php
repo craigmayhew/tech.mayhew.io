473427f172b5059e395e754e2982a1fe250d46c0
Craig Mayhew <craig@mayhew.io>
2020-01-26 19:30:55 +0000

    Corrected path to use TRAVIS_BUILD_DIR env var

--- tools/uploadToS3.php
+++ tools/uploadToS3.php
@@ -3,7 +3,7 @@
 require '../vendor/autoload.php';
 use Aws\Common\Aws;
 
-$dir = '/mnt/c/Users/day2day/Documents/GitHub/tech.mayhew.io/htdocs';
+$dir = getenv('TRAVIS_BUILD_DIR').'/htdocs';
 $bucket = 'tech.mayhew.io';
 $keyPrefix = '';
 
